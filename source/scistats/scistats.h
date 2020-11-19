829cb27bc67a26a79a4bb37418bf8ba9e6d27652
Alan R R Freitas <alandefreitas@gmail.com>
2020-11-19 00:03:16 -0300

    Add single header as optional include

---
+++ source/scistats/scistats.h
@@ -0,0 +1,46 @@
+// This file is generated automatically
+// Do not modify
+#ifndef SCISTATS_SCISTATS_ALL_HEADERS_H
+#define SCISTATS_SCISTATS_ALL_HEADERS_H
+
+#include <scistats/bayesian/bootstrap.h>
+#include <scistats/bayesian/theorem.h>
+#include <scistats/common/concepts.h>
+#include <scistats/common/execution.h>
+#include <scistats/common/type_traits.h>
+#include <scistats/descriptive/bounds.h>
+#include <scistats/descriptive/cov.h>
+#include <scistats/descriptive/descriptive.h>
+#include <scistats/descriptive/max.h>
+#include <scistats/descriptive/mean.h>
+#include <scistats/descriptive/median.h>
+#include <scistats/descriptive/min.h>
+#include <scistats/descriptive/mode.h>
+#include <scistats/descriptive/percentile.h>
+#include <scistats/descriptive/stddev.h>
+#include <scistats/descriptive/var.h>
+#include <scistats/distribution/norm.h>
+#include <scistats/distribution/students_t.h>
+#include <scistats/hypothesis/t_test.h>
+#include <scistats/hypothesis/t_test_interval.h>
+#include <scistats/hypothesis/t_test_paired.h>
+#include <scistats/hypothesis/tail.h>
+#include <scistats/math/abs.h>
+#include <scistats/math/acot.h>
+#include <scistats/math/beta.h>
+#include <scistats/math/beta_inc.h>
+#include <scistats/math/beta_inc_inv.h>
+#include <scistats/math/beta_inc_inv_upper.h>
+#include <scistats/math/beta_inc_upper.h>
+#include <scistats/math/betaln.h>
+#include <scistats/math/constants.h>
+#include <scistats/math/cot.h>
+#include <scistats/math/erfinv.h>
+#include <scistats/math/gammaln.h>
+#include <scistats/math/prod.h>
+#include <scistats/math/sum.h>
+#include <scistats/math/tgamma.h>
+#include <scistats/math/xinbta.h>
+#include <scistats/scistats.h>
+
+#endif // HISTOGRAMS_STATS_H
