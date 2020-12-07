6bb9bc727fecaed2975a270675f3883e83067a75
alandefreitas <alandefreitas@gmail.com>
2020-12-07 17:27:11 -0300

    Update minibench to use timeit

--- source/scistats/time/minibench.h
+++ source/scistats/time/minibench.h
@@ -6,16 +6,15 @@
 #define SCISTATS_MINIBENCH_H
 
 #include <scistats/time/tictoc.h>
+#include <scistats/time/timeit.h>
 #include <vector>
 
 namespace scistats {
-    template<typename CALLABLE, typename DURATION_TYPE = std::chrono::nanoseconds>
+    template<typename DURATION_TYPE = std::chrono::nanoseconds, typename CALLABLE = std::function<void()>>
     std::vector<typename DURATION_TYPE::rep> minibench(CALLABLE fn, size_t replicates = 30) {
         std::vector<typename DURATION_TYPE::rep> results(replicates);
         for (size_t i = 0; i < replicates; ++i) {
-            auto s = tic<DURATION_TYPE>();
-            fn();
-            results[i] = toc<DURATION_TYPE>(s);
+            results[i] = timeit<DURATION_TYPE, CALLABLE>(fn);
         }
         return results;
     }
