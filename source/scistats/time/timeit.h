fc2c6ba74b1dd013af6959e202be729fb6e265d1
alandefreitas <alandefreitas@gmail.com>
2020-12-07 17:26:54 -0300

    Add timeit function

---
+++ source/scistats/time/timeit.h
@@ -0,0 +1,21 @@
+//
+// Created by Alan Freitas on 12/7/20.
+//
+
+#ifndef SCISTATS_TIMEIT_H
+#define SCISTATS_TIMEIT_H
+
+#include <functional>
+#include <scistats/time/tictoc.h>
+
+namespace scistats {
+    template<typename DURATION_TYPE = std::chrono::nanoseconds, typename CALLABLE = std::function<void()>>
+    typename DURATION_TYPE::rep timeit(const CALLABLE &fn) {
+        auto s = tic<DURATION_TYPE>();
+        fn();
+        return toc<DURATION_TYPE>(s);
+    }
+}
+
+
+#endif //SCISTATS_TIMEIT_H
